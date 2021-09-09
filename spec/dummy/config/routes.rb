edebd58178dd2cd6fb9fd099d10685828b6691ae
Karen Lundgren <karen.e.lundgren@gmail.com>
2021-09-09 11:44:55 -0600

    Removing initial ideas to do it with rails g scaffold

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :things
   devise_for :users
-  mount Cookbook::Engine => '/cookbook'
-  resources :doodads
-  root 'doodads#index'
+
+  root 'recipes#index'
 end
